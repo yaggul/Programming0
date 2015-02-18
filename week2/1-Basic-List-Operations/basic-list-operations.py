8eb31759f09f53ea2a5ebd9fe189d4ba799f82c0
yaggul <krommul@gmail.com>
2015-02-18 12:36:50 +0200

    Create basic-list-operations.py

---
+++ week2/1-Basic-List-Operations/basic-list-operations.py
@@ -0,0 +1,40 @@
+[1]
+[1,2,3]
+[]
+[True,True,True]
+["Python","Django"]
+["Dimitur","Blagoev","Kotsev"]
+["Dimitur","Nikoleta","Ekaterina","Elisaveta","Simeon"]
+["Python","PHP"]
+
+languages=["Python","Ruby"]
+languages=languages+["C++"]
+languages=languages+["Java"]
+languages=languages+["C#"]
+languages=languages+["Go"]
+
+languages[0]
+languages[1]
+languages[4]
+
+
+languages[4]="F#"
+languages[len(languages)-1]
+
+
+"Haskell" in languages
+"Ruby" in languages
+"Go" in languages
+"Rust" in languages
+
+
+a=[1,2,3]
+b=[4,5,6]
+c=[1]
+d=[8,8,10]
+
+
+c+b+a+d
+a+b
+c+d
+a+a
