20855641d2559f4510ab50d5aae909400f89892a
yaggul <krommul@gmail.com>
2015-03-03 14:11:55 +0200

    Update and rename week3/2-Resolve-with-Functions to week3/2-Resolve-with-Functions/divisors.py

---
+++ week3/2-Resolve-with-Functions/divisors.py
@@ -0,0 +1,12 @@
+def divisors(x):
+    divs=[]
+    for i in range(1,x-1):
+        if x%i==0:
+            divs+=[i]
+    return divs
+
+
+def main():
+    if __name__=="__main__":
+        main()
+        
