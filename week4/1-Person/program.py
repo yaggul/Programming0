9909be966da8d24b904beae57bf01f616f9b0c10
yaggul <krommul@gmail.com>
2015-03-11 23:34:45 +0200

    Create program.py

---
+++ week4/1-Person/program.py
@@ -0,0 +1,7 @@
+data={}
+data['first_name']=input("Please insert your first name: ")
+data['second_name']=input("Please insert your second name: ")
+data['third_name']=input("Please insert your third name:")
+data['birth_year']=input("Please insert your birth name: ")
+data['current_age']=str(2015-int(data['birth_year']))
+print(data)
