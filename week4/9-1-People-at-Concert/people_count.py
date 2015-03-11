316361c67714bd3b1eb34eddda1d07a5453449f4
yaggul <krommul@gmail.com>
2015-03-11 23:51:09 +0200

    Create people_count.py

---
+++ week4/9-1-People-at-Concert/people_count.py
@@ -0,0 +1,9 @@
+def get_people_count(activity):
+    pcount={}
+    for i in activity:
+        pcount[i]=activity.count(i)
+    return pcount
+
+def main():
+    if __name__=='__main__':
+        main()
